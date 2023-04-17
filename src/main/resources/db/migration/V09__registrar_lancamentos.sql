e9dd78437007705f246327e3354f47cb124ac596
Everton <imevertonrch@gmail.com>
2023-04-17 15:42:59 -0300

    Migração de dados do recurso de Lançamentos

---
+++ src/main/resources/db/migration/V09__registrar_lancamentos.sql
@@ -0,0 +1,10 @@
+INSERT INTO
+    tb_lancamento(descricao,data_vencimento,data_pagamento,valor,observacao,tipo,id_categoria,id_pessoa)
+VALUES
+    ("Salário mensal","2017-06-10", null, 6500.00, "Distribuição de lucros", "RECEITA", 1,1),
+    ("Bahamas", "2017-02-22", "2017-02-22", 100.32, null, "DESPESA", 2, 4),
+    ("Top Club", "2017-06-10", null, 120, null, "RECEITA", 4,2),
+    ("CEMIG", "2017-02-10", "2017-02-10", 110.33, "Geração", "RECEITA", 1,2),
+    ("DMAE", "2017-03-12", null, 200.54, null, "DESPESA", 8, 16),
+    ("Extra", "2017-03-10","2017-03-10", 1010.32, null, "RECEITA", 3, 5),
+    ("Bahamas","2017-06-12", null, 500, null, "RECEITA",2,7);
