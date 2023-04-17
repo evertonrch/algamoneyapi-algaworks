689897e857e8f3b901a7b5040cef9f26bfdea19b
Everton <imevertonrch@gmail.com>
2023-04-17 15:44:50 -0300

    Mapeando modelo Lançamento

---
+++ src/main/java/com/algamoney/api/model/Lancamentos.java
@@ -0,0 +1,67 @@
+package com.algamoney.api.model;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_lancamento")
+public class Lancamentos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String descricao;
+
+    @Column(name = "data_vencimento")
+    private LocalDateTime dataVencimento;
+
+    @Column(name = "data_pagamento")
+    private LocalDateTime datePagamento;
+
+    @Column(scale = 10, precision = 2)
+    private BigDecimal valor;
+
+    @Lob
+    private String observacao;
+
+    @ManyToOne
+    @JoinColumn(name = "id_categoria")
+    private Categoria categoria;
+
+    @ManyToOne
+    @JoinColumn(name = "id_pessoa")
+    private Pessoa pessoa;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public LocalDateTime getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public LocalDateTime getDatePagamento() {
+        return datePagamento;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+}
