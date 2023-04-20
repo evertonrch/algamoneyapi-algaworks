3b6b01cf2d1a7a82d2feb2bcc2dd68539ac14d32
Everton <imevertonrch@gmail.com>
2023-04-19 21:07:43 -0300

    Tratando erros de negócio

--- src/main/java/com/algamoney/api/web/LancamentoResource.java
+++ src/main/java/com/algamoney/api/web/LancamentoResource.java
@@ -1,16 +1,22 @@
 package com.algamoney.api.web;
 
 import com.algamoney.api.event.RecursoCriadoEvent;
+import com.algamoney.api.exception.Error;
 import com.algamoney.api.model.Lancamento;
 import com.algamoney.api.service.LancamentoService;
+import com.algamoney.api.service.exception.PessoaInativaException;
+import com.algamoney.api.service.exception.PessoaInexistenteException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -22,6 +28,9 @@ public class LancamentoResource {
 
     @Autowired
     private ApplicationEventPublisher publisher;
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping
     public ResponseEntity<List<Lancamento>> all() {
@@ -43,5 +52,25 @@ public class LancamentoResource {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(lancamento);
+    }
+
+    @ExceptionHandler({PessoaInativaException.class})
+    public ResponseEntity<Object> handlePessoaInexistenteException(PessoaInativaException ex) {
+        String msgUsuario = messageSource.getMessage("mensagem.pessoa-inativa", null, LocaleContextHolder.getLocale());
+        String msgDesenvolvedor = ex.toString();
+        List<Error> errors = Arrays.asList(new Error(msgUsuario, msgDesenvolvedor));
+        return ResponseEntity
+                .badRequest()
+                .body(errors);
+    }
+
+    @ExceptionHandler({PessoaInexistenteException.class})
+    public ResponseEntity<Object> handlePessoaInexistenteException(PessoaInexistenteException ex) {
+        String msgUsuario = messageSource.getMessage("mensagem.pessoa-inexistente", null, LocaleContextHolder.getLocale());
+        String msgDesenvolvedor = ex.toString();
+        List<Error> errors = Arrays.asList(new Error(msgUsuario, msgDesenvolvedor));
+        return ResponseEntity
+                .badRequest()
+                .body(errors);
     }
 }
