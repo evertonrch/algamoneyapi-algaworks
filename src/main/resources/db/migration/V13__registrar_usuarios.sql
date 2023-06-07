e6663ee6ee6ce683d355b0f4421a6be19ca5460a
Everton <imevertonrch@gmail.com>
2023-06-06 21:09:43 -0300

    Migração DML para usuários, permissões e relacionamento

---
+++ src/main/resources/db/migration/V13__registrar_usuarios.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_usuario(nome, email, senha) VALUES
+                             -- ADMIN
+("admin", "admin@admin.com", "$2y$10$BrQR8Vuxm.trXvXeq5ExKejeZ/HvqyZu89mE/dO8PhU30z7a8uKwS"),
+                            -- 123
+("everton", "everton@algamoney.com", "$2y$10$EubbIkHBZ6XAInrQtssznO5/T3LQqpziYHkMrKUYYyLWBvUoARfYK");
---
+++ src/main/resources/db/migration/V15__registrar_relacionamento_usuario_permissao.sql
@@ -0,0 +1,15 @@
+INSERT INTO tb_usuario_permissao(id_usuario, id_permissao) VALUES
+-- ADMIN
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(1, 5),
+(1, 6),
+(1, 7),
+(1, 8),
+(1, 9),
+-- EVERTON
+(2, 3),
+(2, 6),
+(2, 9);
