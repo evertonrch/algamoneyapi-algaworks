e693f4f6dea16d86248d534b12f256226e423da8
Everton <imevertonrch@gmail.com>
2023-06-09 03:13:50 -0300

    Implementando geração do relatório

--- src/main/java/com/algamoney/api/service/LancamentoService.java
+++ src/main/java/com/algamoney/api/service/LancamentoService.java
@@ -1,5 +1,6 @@
 package com.algamoney.api.service;
 
+import com.algamoney.api.dto.LancamentoEstatisticaPessoa;
 import com.algamoney.api.model.Lancamento;
 import com.algamoney.api.model.Pessoa;
 import com.algamoney.api.repository.LancamentoRepository;
@@ -8,13 +9,22 @@ import com.algamoney.api.repository.filter.LancamentoFilter;
 import com.algamoney.api.repository.projection.ResumoLancamento;
 import com.algamoney.api.service.exception.PessoaInativaException;
 import com.algamoney.api.service.exception.PessoaInexistenteException;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.*;
 
 @Service
 public class LancamentoService {
@@ -53,5 +63,21 @@ public class LancamentoService {
 
     public void deleteLancamento(Long id) {
         lancamentoRepository.deleteById(id);
+    }
+
+    public byte[] relatorioPorPessoa(LocalDate dtInicio, LocalDate dtFim) throws IOException, JRException {
+        List<LancamentoEstatisticaPessoa> dados = lancamentoRepository.porPessoa(dtInicio, dtFim);
+
+        Map<String, Object> parametros = new HashMap<>();
+        parametros.put("DT_INICIO", Date.valueOf(dtInicio)); // sql.Date método que faz parsing de LocalDate p/ Date
+        parametros.put("DT_FIM", Date.valueOf(dtFim)); // sql.Date método que faz parsing de LocalDate p/ Date
+        parametros.put("REPORT_LOCALET", new Locale("pt", "BR"));
+
+        try(InputStream fluxo = this.getClass().getResourceAsStream("/relatorios/lancamentos-por-pessoa.jasper")){
+            JasperPrint jasperPrint = JasperFillManager.fillReport(fluxo, parametros,
+                    new JRBeanCollectionDataSource(dados));
+
+            return JasperExportManager.exportReportToPdf(jasperPrint);
+        }
     }
 }
