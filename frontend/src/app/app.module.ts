cd035d70e6112355a56a0cc20e5c3a7da452b72c
Everton Rocha <imevertonrch@gmail.com>
2024-12-29 01:34:38 -0300

    declarando novos  módulos

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,31 +1,61 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import {InputTextModule} from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea'
+import { InputNumberModule } from 'primeng/inputnumber'
 import {ButtonModule} from 'primeng/button';
 import {TableModule} from 'primeng/table';
 import { TooltipModule } from 'primeng/tooltip';
+import { CalendarModule } from 'primeng/calendar';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputMaskModule } from 'primeng/inputmask'
+import { MessageModule } from 'primeng/message'
+
 import { LancamentosPesquisaComponent } from './components/lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PessoasPesquisaComponent } from './components/pessoas-pesquisa/pessoas-pesquisa.component';
+import { LancamentoCadastroComponent } from './components/lancamento-cadastro/lancamento-cadastro.component';
+import { PessoaCadastroComponent } from './components/pessoa-cadastro/pessoa-cadastro.component';
+import { FormsModule } from '@angular/forms';
+import { MessageComponent } from './components/message/message.component';
+import { LancamentosGridComponent } from './components/lancamentos-grid/lancamentos-grid.component';
+import { PessoasGridComponent } from './components/pessoas-grid/pessoas-grid.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LancamentosPesquisaComponent,
     NavbarComponent,
-    PessoasPesquisaComponent
+    PessoasPesquisaComponent,
+    LancamentoCadastroComponent,
+    PessoaCadastroComponent,
+    MessageComponent,
+    LancamentosGridComponent,
+    PessoasGridComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    MessageModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     InputTextModule,
     ButtonModule,
     TableModule,
-    TooltipModule
+    TooltipModule,
+    InputTextareaModule,
+    CalendarModule,
+    SelectButtonModule,
+    DropdownModule,
+    InputNumberModule,
+    InputMaskModule
   ],
   providers: [],
   bootstrap: [AppComponent]
